c7f9c6c8c2efd50390c0809df682996eccfbfbad
Dmytro Stasenko <3331419+dmyts@users.noreply.github.com>
2025-11-10 16:56:46 +0200

    Clion Makefile support

---
+++ cmdlineargs-plugin/args-provider-clion/src/main/kotlin/com/github/rebel000/cmdlineargs/provider/clion/MakefileRunConfigurationAdapterProvider.kt
@@ -0,0 +1,28 @@
+package com.github.rebel000.cmdlineargs.provider.clion
+
+import com.github.rebel000.cmdlineargs.ArgumentsAdapter
+import com.github.rebel000.cmdlineargs.extensions.ArgumentsAdapterProviderExtension
+import com.intellij.execution.RunnerAndConfigurationSettings
+import com.jetbrains.lang.makefile.MakefileRunConfiguration
+
+class MakefileRunConfigurationAdapterProvider : ArgumentsAdapterProviderExtension {
+    override fun isSupported(s: RunnerAndConfigurationSettings): Boolean {
+        return s.configuration is MakefileRunConfiguration
+    }
+
+    override fun createAdapter(s: RunnerAndConfigurationSettings): ArgumentsAdapter? {
+        return (s.configuration as? MakefileRunConfiguration)?.let { MakefileRunConfigurationAdapter(s) }
+    }
+
+    class MakefileRunConfigurationAdapter(s: RunnerAndConfigurationSettings) : ArgumentsAdapter(s) {
+        override fun getArguments(): String {
+            val config = settings.configuration as? MakefileRunConfiguration ?: return ""
+            return config.arguments
+        }
+
+        override fun setArguments(value: String) {
+            val config = settings.configuration as? MakefileRunConfiguration ?: return
+            config.arguments = value
+        }
+    }
+}
