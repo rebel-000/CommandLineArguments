2ebdfaba145c3c18e691408360ac40b482a17d59
Dmytro Stasenko <3331419+dmyts@users.noreply.github.com>
2025-12-10 17:50:24 +0200

    TrustActionGroup change updatePreview to invalidatePreview

--- cmdlineargs-plugin/args-core/src/main/kotlin/com/github/rebel000/cmdlineargs/actions/TrustActionGroup.kt
+++ cmdlineargs-plugin/args-core/src/main/kotlin/com/github/rebel000/cmdlineargs/actions/TrustActionGroup.kt
@@ -42,7 +42,7 @@ internal class TrustActionGroup : DefaultActionGroup() {
                 }
             }
             if (updatePreview) {
-                service.updatePreview()
+                service.invalidatePreview()
                 service.markDirty()
             }
         }
