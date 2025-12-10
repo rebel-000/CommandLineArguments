391d55a01c837037a33bf82e0278d5d11d78d561
Dmytro Stasenko <3331419+dmyts@users.noreply.github.com>
2025-12-10 17:25:42 +0200

    cleanup TrustActionGroup action

--- cmdlineargs-plugin/args-core/src/main/kotlin/com/github/rebel000/cmdlineargs/actions/TrustActionGroup.kt
+++ cmdlineargs-plugin/args-core/src/main/kotlin/com/github/rebel000/cmdlineargs/actions/TrustActionGroup.kt
@@ -1,7 +1,6 @@
 package com.github.rebel000.cmdlineargs.actions
 
 import com.github.rebel000.cmdlineargs.ArgumentsAdapter
-import com.github.rebel000.cmdlineargs.ArgumentsService
 import com.github.rebel000.cmdlineargs.resources.ActionMessages
 import com.github.rebel000.cmdlineargs.tree.ConfigurationNode
 import com.intellij.openapi.actionSystem.ActionUpdateThread
@@ -27,55 +26,25 @@ internal class TrustActionGroup : DefaultActionGroup() {
     }
 
     abstract class TrustActionBase : DumbAwareToggleAction() {
-        private var selected: Boolean = false
-
-        protected abstract fun isTrusted(adapter: ArgumentsAdapter): Boolean?
         protected abstract fun setTrusted(adapter: ArgumentsAdapter, value: Boolean)
-
         override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
-
-        override fun isSelected(e: AnActionEvent): Boolean {
-            return selected
-        }
-
-        override fun setSelected(e: AnActionEvent, state: Boolean) {
+        override fun setSelected(e: AnActionEvent, state: Boolean) = e.withArgumentDataContext {
             var updatePreview = false
-            val service = ArgumentsService.getInstanceIfCreated(e.project) ?: return
-            val selectionPaths = e.getArgumentDataContext()?.tree?.selectionPaths ?: return
-            for (path in selectionPaths) {
-                val node = path.lastPathComponent as? ConfigurationNode
-                val key = node?.key ?: continue
-                val adapter = service.findAdapter(key) ?: continue
-                setTrusted(adapter, state)
-                node.isEnabled = adapter.isTrusted()
-                updatePreview = true
+            for (path in tree.selectionPaths) {
+                (path.lastPathComponent as? ConfigurationNode)?.let { node ->
+                    node.key
+                        ?.let { service.findAdapter(it) }
+                        ?.let { adapter ->
+                            setTrusted(adapter, state)
+                            node.isEnabled = adapter.isTrusted()
+                            updatePreview = true
+                        }
+                }
             }
-            selected = state
             if (updatePreview) {
                 service.updatePreview()
                 service.markDirty()
             }
-        }
-
-        override fun update(e: AnActionEvent) {
-            val isTrusted: Boolean? = isTrusted(e)
-            e.presentation.isEnabledAndVisible = isTrusted != null
-            selected = isTrusted == true
-            super.update(e)
-        }
-
-        private fun isTrusted(e: AnActionEvent): Boolean? {
-            var hasTrusted = false
-            val service = ArgumentsService.getInstanceIfCreated(e.project) ?: return null
-            val selectionPaths = e.dataContext.getArgumentDataContext()?.tree?.selectionPaths.orEmpty()
-            for (path in selectionPaths) {
-                val key = (path.lastPathComponent as? ConfigurationNode)?.key ?: return null
-                val adapter = service.findAdapter(key) ?: return null
-                val trusted = isTrusted(adapter)
-                if (trusted == false) return false
-                hasTrusted = hasTrusted || trusted == true
-            }
-            return if (hasTrusted) true else null
         }
     }
 
@@ -84,7 +53,7 @@ internal class TrustActionGroup : DefaultActionGroup() {
             templatePresentation.keepPopupOnPerform = KeepPopupOnPerform.Never
             templatePresentation.text = ActionMessages.message("action.cmdlineargs.trust.name.text")
         }
-        override fun isTrusted(adapter: ArgumentsAdapter): Boolean? = adapter.isTrustedByName()
+        override fun isSelected(e: AnActionEvent): Boolean = e.withArgumentDataContext(false) { treeIsTrustedByName }
         override fun setTrusted(adapter: ArgumentsAdapter, value: Boolean) = adapter.setTrustedByName(value)
     }
 
@@ -93,7 +62,7 @@ internal class TrustActionGroup : DefaultActionGroup() {
             templatePresentation.keepPopupOnPerform = KeepPopupOnPerform.Never
             templatePresentation.text = ActionMessages.message("action.cmdlineargs.trust.type.text")
         }
-        override fun isTrusted(adapter: ArgumentsAdapter): Boolean? = adapter.isTrustedByType()
+        override fun isSelected(e: AnActionEvent): Boolean = e.withArgumentDataContext(false) { treeIsTrustedByType }
         override fun setTrusted(adapter: ArgumentsAdapter, value: Boolean) = adapter.setTrustedByType(value)
     }
 }
